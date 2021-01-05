e9c62fb639e37388796201b9d357a92893995777
EricSOliveira <contato.ericoliveira@gmail.com>
2021-01-05 17:10:34 -0300

    subindo app

---
+++ src/components/Hearder/index.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import logo from '../../assets/logo.svg';
+import './style.css';
+
+function Hearder() {
+  const reserveSize = useSelector(state => state.reserve.length)
+
+  return (
+    <header className="container">
+      
+      <Link to="/">
+        <img className="logo" src={logo} alt="Logo" />
+      </Link>
+
+      <Link className="reserva" to="/reservas">
+        <div>
+          <strong>Minhas reservas</strong>
+          <span>{reserveSize} reservas</span>
+        </div>
+      </Link>
+    </header>
+  )
+}
+
+export default Hearder;
